e7c6c04573182f8904c3214d29ff7ea21beebce8
robertexe <robtabares92@gmail.com>
2018-06-06 15:45:30 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    words_array = self.split(" ")
+    counter = 0
 
+    words_array.map do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        counter += 1
+      end
+    end
+    counter
   end
 end
+
+str = "Hi hello! goodbye bye?"
+puts str.count_sentences
