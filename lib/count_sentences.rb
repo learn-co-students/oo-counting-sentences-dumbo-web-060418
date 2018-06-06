0a0a707cc3150dd4f360babc31b84efd54003dde
QMaximillian <quinnlashinsky@gmail.com>
2018-06-06 19:03:32 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,46 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    	true
+    else
+    	false
+    end
   end
 
   def question?
-
+  	if self.end_with?("?")
+  		true
+  	else
+  		false
+  	end
   end
 
   def exclamation?
-
+  	if self.end_with?("!")
+  		true
+  	else
+  		false
+  	end
   end
 
   def count_sentences
+  	
+  	phrase = self.split(/[.?!]/).delete_if(&:empty?)
+  	phrase_count = phrase.count
+  	phrase_count
 
-  end
+
+  	# if self.split.count == 0
+  	# 	0
+  	# elsif self.sentence? || self.question? || self.exclamation?
+  	# 	self.split.count
+  		
+  	end
+
+
+  
 end
+
+
+# Pry.start
